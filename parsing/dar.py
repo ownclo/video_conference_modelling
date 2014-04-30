3e7206e917145722fc17aa2735f6a84c98e3a511
Ownclo - Arcis <mrownclo@gmail.com>
2014-04-30 22:37:49 +0400

    Implemented DAR(p). XXX: Bug in corr matrix generation

--- parsing/dar.py
+++ parsing/dar.py
@@ -1,23 +1,77 @@
 import numpy as np
+from scipy.stats import rv_discrete
+from scipy.stats import nbinom
 
+def neg_bin(mean, var):
+    # Where does this is explained?
+    p = mean / var
+    n = mean * p / (1 - p)
+
+    while(True):
+        yield nbinom.rvs(n, p)
+
+def dar1(cor1, mean, var):
+    neg_bin_seq = neg_bin(mean, var)
+    prev = neg_bin_seq.next()
+
+    while(True):
+        if random.random() > cor1:
+            prev = neg_bin_seq.next()
+        yield prev
+
+# will work for dar(2), won't for dar(3) and higher
 def corrmatrix(corrs):
     init = np.insert(corrs[:-1], 0, 1)
 
     res = []
+    # XXX: There is a bug here. Matrix does not shift,
+    # it grows from diagonal to edges
     for i in xrange(corrs.size):
         res.append(np.roll(init, i))
 
     return np.array(res)
 
 
-if __name__ == "__main__":
-    cor1 = 0.4
-    cor2 = 0.3
-
-    corrs = np.array([cor1, cor2])
+def estimate_darp_probs(corrs):
     corrm = corrmatrix(corrs)
-
     xs = np.linalg.solve(corrm, corrs)
 
-    print xs
-    print np.allclose(np.dot(corrm,xs), corrs)
+    p = sum(xs)
+    alphas = [ x / p for x in xs ]
+    return p, alphas
+
+def push_sliding_window(window, item):
+    del window[-1]
+    window.insert(0, item)
+
+def dar_p(corrs, mean, var):
+    p, alphas = estimate_darp_probs(corrs)
+
+    neg_bin_seq = neg_bin(mean, var)
+    prev_window = [neg_bin_seq.next(), neg_bin_seq.next()]
+
+    prev_dist = rv_discrete(name='prev_dist', values=(range(2), alphas))
+    curr_dist = rv_discrete(name='curr_dist', values=([True, False], [p, 1.0 - p]))
+
+    while(True):
+        if curr_dist.rvs(): # take from history
+            index = prev_dist.rvs()
+            frame = prev_window[index]
+
+            push_sliding_window(prev_window, frame)
+            yield frame
+
+        else: # generate new one
+            frame = neg_bin_seq.next()
+            push_sliding_window(prev_window, frame)
+            yield frame
+
+
+if __name__ == "__main__":
+    cor1 = 0.89
+    cor2 = 0.7999
+
+    corrs = np.array([cor1, cor2])
+
+    print estimate_darp_probs(corrs)
+    print dar_p(corrs, 3287.0, 107880.85923).next()
--- parsing/parse_trace.py
+++ parsing/parse_trace.py
@@ -6,22 +6,8 @@ from scipy.stats import nbinom
 from scipy.stats import moment
 import random
 
-def neg_bin(mean, var):
-    # Where does this is explained?
-    p = mean / var
-    n = mean * p / (1 - p)
-
-    while(True):
-        yield nbinom.rvs(n, p)
-
-def dar(cor1, mean, var):
-    neg_bin_seq = neg_bin(mean, var)
-    prev = neg_bin_seq.next()
-
-    while(True):
-        if random.random() > cor1:
-            prev = neg_bin_seq.next()
-        yield prev
+import pyeeg
+import dar
 
 def take(n, seq):
     result = []
@@ -55,6 +41,7 @@ def acf(x):
 if __name__ == "__main__":
     tracefile = sys.argv[1]
     with open(tracefile) as f:
+        # NOTE: first frame must be skipped
         sizes = numpy.array(get_sizes(f)[1:])
 
     mean = sizes.mean()
@@ -69,9 +56,12 @@ if __name__ == "__main__":
     #     print i, size
     #     i += 1
 
-    # # CORRELATION COEFFICIENTS OVER LAG (ACF)
-    # for i in xrange(0, 100):
-    #     print i, corr[i]
+    # CORRELATION COEFFICIENTS OVER LAG (ACF)
+    for i in xrange(0, 5):
+        print i, corr[i]
+
+    print corr[0:5]
+    print pyeeg.hurst(sizes)
 
     # HISTOGRAM
     # hist, edges = numpy.histogram(sizes, density=True)
@@ -80,23 +70,23 @@ if __name__ == "__main__":
     #     print e, h
 
     #print "N:", sizes.size
-    #print "Mean:", mean
-    #print "Variance:", var
+    print "Mean:", mean
+    print "Variance:", var
 
     #print "Autocorrelation (shift = 1):", cor1
     #print "Autocorrelation (shift = 2):", corr[2]
 
     # Testing DAR distribution
-    dar_seq = numpy.array(take(10000, dar(cor1, mean, var)))
+    #dar_seq = numpy.array(take(10000, dar.dar1(cor1, mean, var)))
     #hs, es = numpy.histogram(dar_seq, density=True)
     #for e, h in zip(es, hs):
         #print e, h
 
     #print "DAR(1) mean, var: ", dar_seq.mean(), dar_seq.var()
-    dar_acf = acf(dar_seq)
-    sizes_acf = acf(sizes)
-    for i in xrange(0, 20):
-        print i, sizes_acf[i], dar_acf[i]
+    #dar_acf = acf(dar_seq)
+    #sizes_acf = acf(sizes)
+    #for i in xrange(0, 20):
+    #    print i, sizes_acf[i], dar_acf[i]
 
     #print "\n"
     #for i in xrange(2, 7):
@@ -104,3 +94,15 @@ if __name__ == "__main__":
         #mmodel = moment(dar_seq, i)
 
         #print morig, mmodel, morig/mmodel
+
+    sample_corrs = numpy.array([0.89, 0.7999])
+    sample_mean = 3287.0
+    sample_var = 107880.85923
+
+    #dar_generator = dar.dar_p(sample_corrs, sample_mean, sample_var)
+    dar_generator = dar.dar_p(corr[1:3], mean, var)
+    dar2_seq = numpy.array(take(10000, dar_generator))
+
+    corr_dar2 = acf(dar2_seq)
+    print "DAR2 cor1, cor2: ", corr_dar2[0:5]
+    print "DAR2 mean, var:  ", dar2_seq.mean(), dar2_seq.var()
